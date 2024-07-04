84e06e3f52a4ac2573c2b5eb0b0ed69731584b21
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2024-07-04 13:54:36 +0530

    show all user cards

--- src/app/members/layout.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Layout = () => {
-  return <div>Nested Layout</div>;
-};
-
-export default Layout;
--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,10 +1,15 @@
+import { getMembers } from "@/src/actions/memberActions";
 import Link from "next/link";
+import MemberCard from "./MemberCard";
 
-const Members = () => {
+const Members = async () => {
+  const members = await getMembers();
   return (
-    <div>
-      <h3>Members Page</h3>
-      <Link href={"./"}>Home</Link>
+    <div className="mt-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-8">
+      {members &&
+        members.map((member) => {
+          return <MemberCard member={member} key={member.id} />;
+        })}
     </div>
   );
 };
