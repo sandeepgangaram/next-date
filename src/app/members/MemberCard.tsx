dacf76f2fff9c186a8e1eeb1e6f9001d5190c12f
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2024-07-17 21:56:14 +0530

    add loading spinner for like

--- src/app/members/MemberCard.tsx
+++ src/app/members/MemberCard.tsx
@@ -1,22 +1,37 @@
 "use client";
+import { toggleLikeMember } from "@/src/actions/likeActions";
 import { calculateAge, transformImageUrl } from "@/src/actions/util";
 import LikeButton from "@/src/components/LikeButton";
 import PresenceDot from "@/src/components/PresenceDot";
 import { Card, CardFooter, Image } from "@nextui-org/react";
 import { Member } from "@prisma/client";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   member: Member;
   likeIds: string[];
 }
 const MemberCard = ({ member, likeIds }: Props) => {
-  const hasLiked = likeIds.includes(member.userId);
+  const [hasLiked, setHasLiked] = useState(likeIds.includes(member.userId));
+  const [loading, setLoading] = useState(false);
+
   const preventLikeAction = (e: React.MouseEvent) => {
     e.preventDefault();
     e.stopPropagation();
   };
+
+  async function toggleLike() {
+    setLoading(true);
+    try {
+      await toggleLikeMember(member.userId, hasLiked);
+      setHasLiked(!hasLiked);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
     <Card fullWidth as={Link} href={`/members/${member.userId}`} isPressable>
       <Image
@@ -28,7 +43,11 @@ const MemberCard = ({ member, likeIds }: Props) => {
       />
       <div onClick={preventLikeAction}>
         <div className="absolute top-3 right-3 z-50">
-          <LikeButton targetId={member.userId} hasLiked={hasLiked} />
+          <LikeButton
+            loading={loading}
+            toggleLike={toggleLike}
+            hasLiked={hasLiked}
+          />
         </div>
         <div className="absolute top-2 left-3 z-50">
           <PresenceDot member={member} />
--- src/components/LikeButton.tsx
+++ src/components/LikeButton.tsx
@@ -1,36 +1,35 @@
 "use client";
 
-import { useRouter } from "next/navigation";
 import React from "react";
-import { toggleLikeMember } from "../actions/likeActions";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
+import { PiSpinnerGap } from "react-icons/pi";
 
 interface Props {
-  targetId: string;
+  loading: boolean;
   hasLiked: boolean;
+  toggleLike: () => void;
 }
-const LikeButton = ({ targetId, hasLiked }: Props) => {
-  const router = useRouter();
-
-  async function toggleLike() {
-    console.log("on heart click");
-    await toggleLikeMember(targetId, hasLiked);
-    router.refresh();
-  }
+const LikeButton = ({ loading, hasLiked, toggleLike }: Props) => {
   return (
-    <div
-      onClick={toggleLike}
-      className="relative hover:opacity-80 transition cursor-pointer"
-    >
-      <AiOutlineHeart
-        size={28}
-        className="fill-white absolute -top-[2px] -right-2[px]"
-      />
-      <AiFillHeart
-        size={24}
-        className={hasLiked ? "fill-rose-500" : "fill-neutral-500/70"}
-      />
-    </div>
+    <>
+      {loading ? (
+        <PiSpinnerGap size={32} className="fill-white animate-spin" />
+      ) : (
+        <div
+          onClick={toggleLike}
+          className="relative hover:opacity-80 transition cursor-pointer"
+        >
+          <AiOutlineHeart
+            size={28}
+            className="fill-white absolute -top-[2px] -right-2[px]"
+          />
+          <AiFillHeart
+            size={24}
+            className={hasLiked ? "fill-rose-500" : "fill-neutral-500/70"}
+          />
+        </div>
+      )}
+    </>
   );
 };
 
